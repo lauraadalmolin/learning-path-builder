2fbd65426b0fda31c5752a32dc29a61db4d23aaa
Laura A. Dalmolin <laura.aguiar.dalmolin@gmail.com>
2023-01-24 20:35:40 -0300

    remove weights and update variable name

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
 import '../styles/globals.css';
 import { Inter } from '@next/font/google';
 
-const montserrat = Inter({ subsets: ['latin'], weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'] });
+const inter = Inter({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }) {
-  return <main className={montserrat.className}>
+  return <main className={inter.className}>
       <Component {...pageProps} />
     </main>;
 }
